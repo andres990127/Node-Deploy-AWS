91485d382d7efa34e28fb6548746c2d5fd1aea74
Andrés Mauricio Gutiérrez Rojas <andres_mau.gutierrez@uao.edu.co>
2023-02-12 15:50:17 -0500

    Agregando acceso HTTP

--- app.js
+++ app.js
@@ -1,25 +1,40 @@
 // Módulos requeridos
 const express = require('express');
+const fs = require('fs');
 const http = require('http');
+const https = require('https');
 const helmet = require('helmet');
-let compression = require('compression');
+var compression = require('compression');
 require('dotenv').config();
 const {v4: uuidv4} = require('uuid');
-let cors = require("cors");
+const cors = require('cors');
 
+// Opciones para el servidor https, para usar los certificados para SSL/TLS
+const httpsServerOptions = {
+    key: fs.readFileSync(process.env.KEY_PATH),
+    cert: fs.readFileSync(process.env.CERT_PATH),
+};
 
 const app = express();
 app.use(helmet({contentSecurityPolicy: false}));  // Ayuda a proteger aplicaciones Express
-app.use(compression()); // Comprime las respuestas para más rapidez
+app.use(compression());
 app.use(cors());
 
 // Servidor HTTP
 const serverHttp = http.createServer(app);
 serverHttp.listen(process.env.HTTP_PORT, process.env.IP);
 
+// Servidor HTTPS
+const serverHttps = https.createServer(httpsServerOptions, app);
+serverHttps.listen(process.env.HTTPS_PORT, process.env.IP);
+
+// Redireccionamiento de http a https, debe ser el primer app.use
+app.use((req, res, next) => {
+    if (req.secure) next(); else res.redirect(`https://${req.headers.host}${req.url}`);
+});
+
 // Contenido estático
 app.use(express.static('./public'));
-
 
 // API
 app.get('/api/get-uuid', function(req, res) {
@@ -28,9 +43,7 @@ app.get('/api/get-uuid', function(req, res) {
 
 // 404
 app.get('*', function(req, res) {
-    res.status(404).send('Error 404 - Recurso no encontrado')
+    res.status(404).send('Error 404 - Recurso no encontrado');
 });
-
-
 
 
